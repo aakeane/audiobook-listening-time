bc7dd799f45bdeda902cec690b14bfac3c5e4291
A. Keane <aaronjkeane@gmail.com>
2021-01-11 20:45:30 +0000

    change output of results

--- app.js
+++ app.js
@@ -9,43 +9,45 @@ function totalTime(e) {
 
   let playS = document.querySelector("#playback-speed");
   let hr = document.querySelector("#hours");
-  let min = document.querySelector("#minutes");
+  let mins = document.querySelector("#minutes");
+  // Store original hour, mins & playback speed for the output
+  const arr = [hr.value, mins.value, playS.value];
 
   // Authenticate that no fields are empty before calculation
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].value.length === 0) {
+    if (elements[i].value === "") {
       return;
     }
   }
 
   let playbackSpeed = playS.value;
   let hours = hr.value * 60;
-  let minutes = min.value;
+  let minutes = mins.value;
 
   // If speed is faster than 1x
   if (playbackSpeed >= 1) {
     const totalHours = hours / playbackSpeed;
     const totalMins = minutes / playbackSpeed;
     const totalTime = totalHours + totalMins;
-    outputInfo(totalTime);
+    outputInfo(totalTime, arr);
 
     // If speed is slower than 1x
   } else {
     const totalHours = hours / playbackSpeed;
     const totalMins = minutes / playbackSpeed;
     const totalTime = totalHours + totalMins;
-    outputInfo(totalTime);
+    outputInfo(totalTime, arr);
   }
 }
 
 // Convert total minutes to hours & minutes
-function outputInfo(time) {
+function outputInfo(time, arr) {
   const hours = time / 60;
   const rhours = Math.floor(hours);
   const minutes = (hours - rhours) * 60;
   const rminutes = Math.round(minutes);
 
-  document.querySelector("#output").textContent = `${Math.floor(
-    time
-  )} minutes = ${rhours} hour(s) and ${rminutes} minute(s)`;
+  document.querySelector(
+    "#output"
+  ).innerHTML = `<p class="fw-lighter fst-italic fs-3">${arr[0]} hours ${arr[1]} mins at ${arr[2]}x =</em></p> <p class="display-6">${rhours} hour(s) and ${rminutes} minute(s)</p`;
 }
